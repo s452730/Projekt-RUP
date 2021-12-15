6cf38c1668edacda8a68392d199955997f38cd9a
Ekinto <alitra@st.amu.edu.pl>
2021-12-15 01:26:54 +0100

    working trace API

--- app/src/main/java/com/example/wearapp/MainActivity.java
+++ app/src/main/java/com/example/wearapp/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.wearapp;
 
 import ClothingService.ClothingService;
 import android.os.AsyncTask;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.FileUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,24 +17,16 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
-import java.io.FileWriter;
-import java.util.Locale;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,151 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private String homeAddress;
     private String workAddress;
     private int hourOfWorkingStart;
-    private List<String> data;
+    private List<String> data = new ArrayList<>();
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        adresM = (EditText) findViewById(R.id.adresM);
-        adresP = (EditText) findViewById(R.id.adresP);
-        godzina = (EditText) findViewById(R.id.godzina);
-        txtShow = (TextView) findViewById(R.id.textTest);
-        button = (Button) findViewById(R.id.button);
-        button2 = (Button) findViewById(R.id.button2);
-
-
-        //dla uruchomenia api tras -> new TraceData.start(), resultat w trace_json
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String myTxt = adresM.getText().toString() + "\n" + adresP.getText().toString() + "\n" + godzina.getText().toString() + "\n";
-                try {
-
-//                    FileOutputStream fileOut = openFileOutput("test.json",MODE_PRIVATE);
-//                    fileOut.write(myTxt.getBytes(StandardCharsets.UTF_8));
-//                    fileOut.close();
-//                    adresM.setText("");
-//                    adresP.setText("");
-//                    godzina.setText("");
-
-
-                    homeAddress = adresM.getText().toString();
-                    workAddress = adresP.getText().toString();
-                    hourOfWorkingStart = Integer.parseInt(godzina.getText().toString());
-                    new TraceData().start();
-
-
-                    Toast.makeText(MainActivity.this, data.get(0), Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    FileInputStream fileIn = openFileInput("test.json");
-                    InputStreamReader reader = new InputStreamReader(fileIn);
-                    BufferedReader bufferread = new BufferedReader(reader);
-                    StringBuffer strbuff = new StringBuffer();
-                    String str;
-                    while ((str = bufferread.readLine()) != null) {
-                        strbuff.append(str + "\n");
-                    }
-                    //OPTIONS
-                    // TOMORROW_DATE
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.add(Calendar.DAY_OF_YEAR, 1);
-                    Date tomorrow = calendar.getTime();
-                    String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(tomorrow);
-                    String city = "Poznan";
-                    // String hour = "0";
-                    String key = "992e7cab06164165980213921210812";
-                    String url = "https://api.weatherapi.com/v1/history.json?key=" + key +
-                            "&q=" + city + "&dt=" + date;
-                    new WeatherAPI().execute(url);
-                    txtShow.setText(strbuff.toString());
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-    }
-
-    class TraceData extends Thread {
-
-        public void getTraceByJson(String url) throws JSONException {
-            //zwraca czas, kiedy trzeba wyjsc z domu i trase
-            JSONObject info = null;
-            info = new JSONObject(url);
-            JSONObject departure_time = null;
-            JSONObject line_transport = null;
-            departure_time = info.getJSONArray("routes")
-                    .getJSONObject(0)
-                    .getJSONArray("legs")
-                    .getJSONObject(0)
-                    .getJSONObject("departure_time");
-
-
-            line_transport = info.getJSONArray("routes")
-                    .getJSONObject(0)
-                    .getJSONArray("legs")
-                    .getJSONObject(0)
-                    .getJSONArray("steps")
-                    .getJSONObject(1)
-                    .getJSONObject("transit_details");
-
-            data.add(departure_time.toString());
-            data.add(line_transport.toString());
-        }
-
-        @Override
-        public void run() {
-            try {
-//              URL url = new URL("https://maps.googleapis.com/maps/api/directions" +
-//                        "/json?key=AIzaSyCNx1cp5ReJvuzJ5XqCBijNxy2B0mAUl_s&mode=transit&origin=" + homeAddress
-//                        + "&destination=" + workAddress
-//                        + "&arrival_time=" + hourOfWorkingStart);
-                //test
-                URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=Os.SobieskiegoPoznan&destination=Drużbickiego2,Poznań&key=AIzaSyCNx1cp5ReJvuzJ5XqCBijNxy2B0mAUl_s&arrival_time=1639428974");
-
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                connection.setRequestMethod("GET");
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine()) != null)
-                    buffer.append(line).append("\n");
-
-                String trace_data = buffer.toString();
-                getTraceByJson(trace_data);
-
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    public class WeatherAPI extends AsyncTask<String, String, String> {
+    class WeatherAPI extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... strings) {
@@ -272,5 +123,148 @@ public class MainActivity extends AppCompatActivity {
             int time = clothes.getTime();
             txtShow.setText(cloth + time);
         }
+    }
+
+    class TraceData extends Thread {
+
+        public void getTraceByJson(String url) throws JSONException {
+            //zwraca czas, kiedy trzeba wyjsc z domu i trase
+            JSONObject info = null;
+            info = new JSONObject(url);
+            String departure_time = "";
+            String line_transport = "";
+            departure_time = info.getJSONArray("routes")
+                    .getJSONObject(0)
+                    .getJSONArray("legs")
+                    .getJSONObject(0)
+                    .getJSONObject("departure_time")
+                    .getString("value");
+            line_transport = info.getJSONArray("routes")
+                    .getJSONObject(0)
+                    .getJSONArray("legs")
+                    .getJSONObject(0)
+                    .getJSONArray("steps")
+                    .getJSONObject(1)
+                    .getJSONObject("transit_details")
+                    .getJSONObject("line")
+                    .getString("short_name");
+            data.add(departure_time);
+            data.add(line_transport);
+        }
+
+        @Override
+        public void run() {
+            try {
+//              URL url = new URL("https://maps.googleapis.com/maps/api/directions" +
+//                        "/json?key=AIzaSyCNx1cp5ReJvuzJ5XqCBijNxy2B0mAUl_s&mode=transit&origin=" + homeAddress
+//                        + "&destination=" + workAddress
+//                        + "&arrival_time=" + hourOfWorkingStart);
+                //test
+                URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=Os.SobieskiegoPoznan&destination=Druzbickiego2,Poznan&key=AIzaSyCNx1cp5ReJvuzJ5XqCBijNxy2B0mAUl_s&mode=transit&arrival_time=1639428974");
+                //URL url = new URL("https://maps.googleapis.com/maps/api/directions/json?origin=Os.SobieskiegoPoznan&destination=Drużbickiego2,Poznań&key=AIzaSyCNx1cp5ReJvuzJ5XqCBijNxy2B0mAUl_s&arrival_time=1639428974");
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null)
+                    buffer.append(line).append("\n");
+
+                String trace_data = buffer.toString();
+                getTraceByJson(trace_data);
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adresM = (EditText) findViewById(R.id.adresM);
+        adresP = (EditText) findViewById(R.id.adresP);
+        godzina = (EditText) findViewById(R.id.godzina);
+        txtShow = (TextView) findViewById(R.id.textTest);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+
+
+        //dla uruchomenia api tras -> new TraceData.start(), resultat w trace_json
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String myTxt = adresM.getText().toString() + "\n" + adresP.getText().toString() + "\n" + godzina.getText().toString() + "\n";
+                try {
+
+//                    FileOutputStream fileOut = openFileOutput("test.json",MODE_PRIVATE);
+//                    fileOut.write(myTxt.getBytes(StandardCharsets.UTF_8));
+//                    fileOut.close();
+//                    adresM.setText("");
+//                    adresP.setText("");
+//                    godzina.setText("");
+
+
+                    homeAddress = adresM.getText().toString();
+                    workAddress = adresP.getText().toString();
+                    hourOfWorkingStart = Integer.parseInt(godzina.getText().toString());
+                    new TraceData().start();
+
+
+                    Toast.makeText(MainActivity.this, data.get(0), Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileInputStream fileIn = openFileInput("test.json");
+                    InputStreamReader reader = new InputStreamReader(fileIn);
+                    BufferedReader bufferread = new BufferedReader(reader);
+                    StringBuffer strbuff = new StringBuffer();
+                    String str;
+                    while ((str = bufferread.readLine()) != null) {
+                        strbuff.append(str + "\n");
+                    }
+                    //OPTIONS
+                    //TOMORROW_DATE
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.DAY_OF_YEAR, 1);
+                    Date tomorrow = calendar.getTime();
+                    String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(tomorrow);
+                    String city = "Poznan";
+                    //String hour = "0";
+                    String key = "992e7cab06164165980213921210812";
+                    String url = "https://api.weatherapi.com/v1/history.json?key=" + key +
+                            "&q=" + city + "&dt=" + date;
+                    new WeatherAPI().execute(url);
+                    //txtShow.setText(strbuff.toString());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
